ea8aebc5081dbbc63f518dad0444af7c6fb63505
LucasDotCampos <lucasdotcampos@gmail.com>
2021-09-09 16:27:39 -0300

    Add files via upload

---
+++ src/routes.ts
@@ -0,0 +1,12 @@
+import  { Router }from 'express'
+import AuthController from './app/controllers/AuthController';
+import UserController from './app/controllers/UserController';
+
+
+
+const routes = Router();
+
+routes.post('/users', UserController.store);
+routes.post('/auth', AuthController.authenticate);
+
+export default routes;

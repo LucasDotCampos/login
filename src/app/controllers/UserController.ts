5d5eb7bb2301c7970ff5a853f91ef0e5b88382d2
Lucas Campos <lucasdotcampos@gmail.com>
2021-11-13 15:54:22 -0400

    rotas

--- src/app/controllers/UserController.ts
+++ src/app/controllers/UserController.ts
@@ -1,17 +1,16 @@
-import { Request, Response } from 'express'
-import { getRepository } from 'typeorm';
+import { Request, Response } from "express";
+import { getRepository } from "typeorm";
 
-import { User } from '../models/User'
-
+import { User } from "../models/User";
 
 class UserController {
   async store(request: Request, response: Response) {
-    console.log('chegou no users')
+    console.log("chegou no users");
     const repository = getRepository(User);
 
-    const { email, password } = request.body
+    const { email, password } = request.body;
 
-    const userExists =  await repository.findOne({ where: { email } });
+    const userExists = await repository.findOne({ where: { email } });
 
     if (userExists) {
       return response.sendStatus(409);
@@ -20,9 +19,16 @@ class UserController {
     const user = repository.create({ email, password });
     await repository.save(user);
 
-    return response.json(user)
+    return response.json(user);
+  }
 
-    
+  async get(request: Request, response: Response) {
+    const repository = getRepository(User);
+    const index = await repository.find({
+      select: ["email"],
+    });
+
+    return response.json(index);
   }
 }
 
--- src/routes.ts
+++ src/routes.ts
@@ -1,12 +1,10 @@
-import  { Router }from 'express'
-import AuthController from './app/controllers/AuthController';
-import UserController from './app/controllers/UserController';
-
-
+import { Router } from "express";
+import AuthController from "./app/controllers/AuthController";
+import UserController from "./app/controllers/UserController";
 
 const routes = Router();
 
-routes.post('/users', UserController.store);
-routes.post('/auth', AuthController.authenticate);
-
+routes.post("/users", UserController.store);
+routes.post("/auth", AuthController.authenticate);
+routes.get("/lista", UserController.get);
 export default routes;
